d83c83b75219ce596792b0f88218f32f6ccd1ba9
nicholasmmaratta <103220414+nicholasmmaratta@users.noreply.github.com>
2022-07-07 16:51:03 -0400

    Camel case new edit (#3)
    * git ignored node modules and itself
    
    * added main script & random user
    
    * added everything remaining
    
    * added toCamelCase to index file
    
    * another short update to use the or in the function
    git status

--- src/replaceAndCamelCase.js
+++ src/replaceAndCamelCase.js
@@ -2,15 +2,15 @@ function toCamelCase(str){
     str = str.split("")
     for (let i = 0; i < str.length; i++) {
     console.log(str, str[i]) 
-    if(str[i] == "-") {
+    if(str[i] == "-" || str[i] == "_") {
       str.splice(i, 1)
       str[i] = str[i].toUpperCase()
       
     }
-    if(str[i] == "_") {
-      str.splice(i, 1)
-      str[i] = str[i].toUpperCase()
-    } 
+    // if(str[i] == "_") {
+    //   str.splice(i, 1)
+    //   str[i] = str[i].toUpperCase()
+    // } 
   }
     
   return str.join("")
